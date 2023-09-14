e2457813f2ab6e361288af1b277f632d35b178c0
RyukillerZ <42920480+RyukillerZ@users.noreply.github.com>
2023-09-14 15:03:09 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -38,24 +38,47 @@ tlsStatus=${remainingDays}
 if [[ ${remainingDays} -le 0 ]]; then
 	tlsStatus="expired"
 fi
+
+# TOTAL ACC CREATE VMESS WS
+vmess=$(grep -c -E "^###&" "/etc/xray/config.json")
+# TOTAL ACC CREATE  VLESS WS
+vless=$(grep -c -E "^#&#" "/etc/xray/config.json")
+# TOTAL ACC CREATE  TROJAN WS TLS
+trws=$(grep -c -E "^#!~" "/etc/xray/config.json")
+
+# Get the name of the network interface
+interface=$(vnstat --iflist | awk '{print $3}')
+
+# Get the traffic statistics for today, yesterday, and the current month
+dtoday="$(vnstat -i "${interface}" | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
+utoday="$(vnstat -i "${interface}" | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
+ttoday="$(vnstat -i "${interface}" | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
+dyest="$(vnstat -i "${interface}" | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
+uyest="$(vnstat -i "${interface}" | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
+tyest="$(vnstat -i "${interface}" | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
+dmon="$(vnstat -i "${interface}" -m | grep "$(date +"%b '%y")" | awk '{print $3" "substr ($4, 1, 1)}')"
+umon="$(vnstat -i "${interface}" -m | grep "$(date +"%b '%y")" | awk '{print $6" "substr ($7, 1, 1)}')"
+tmon="$(vnstat -i "${interface}" -m | grep "$(date +"%b '%y")" | awk '{print $9" "substr ($10, 1, 1)}')"
+
 # OS Uptime
 uptime="$(uptime -p | cut -d " " -f 2-10)"
 # Download
 #Download/Upload today
-dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
-utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
-ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
+#dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
+#utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
+#ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
 #Download/Upload yesterday
-dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
-uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
-tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
+#dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
+#uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
+#tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
 #Download/Upload current month
-dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')"
-umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
-tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
+#dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')"
+#umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
+#tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
+
 # user
 Exp2=$"Lifetime"
-Name=$"TIM3VPN"
+Name=$"givpn"
 # Getting CPU Information
 cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
 cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
@@ -75,27 +98,47 @@ tram=$( free -m | awk 'NR==2 {print $2}' )
 uram=$( free -m | awk 'NR==2 {print $3}' )
 fram=$( free -m | awk 'NR==2 {print $4}' )
 clear 
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;34m                      VPS INFO                    \e[0m"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;32m OS            \e[0m: "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	
-echo -e "\e[1;32m Uptime        \e[0m: $uptime"
-echo -e "\e[1;32m Public IP     \e[0m: $IPVPS"
-echo -e "\e[1;32m Country       \e[0m: $LOC"
-#echo -e "\e[1;32m ASN           \e[0m: $ISP"
-#echo -e "\e[1;32m CITY          \e[0m: $CITY"
-echo -e "\e[1;32m DOMAIN        \e[0m: $domain"	
-echo -e "\e[1;32m DATE & TIME   \e[0m: $DATE2"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;34m                      RAM INFO                    \e[0m"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
 echo -e ""
-echo -e "\e[1;32m RAM USED   \e[0m: $uram MB"	
-echo -e "\e[1;32m RAM TOTAL  \e[0m: $tram MB"
+echo -e "\e[$text VPS Script"
+echo -e " \e[$line╒════════════════════════════════════════════════════════════╕\e[m"
+echo -e "  \e[$back_text                    \e[30m[\e[$box SERVER INFORMATION\e[30m ]\e[1m                  \e[m"
+echo -e " \e[$line╘════════════════════════════════════════════════════════════╛\e[m"
+echo -e "  \e[$text Cpu Model            :$cname"
+echo -e "  \e[$text Cpu Frequency        :$freq MHz"
+echo -e "  \e[$text Number Of Core       : $cores"
+echo -e "  \e[$text CPU Usage            : $cpu_usage"
+echo -e "  \e[$text Operating System     : "$(hostnamectl | grep "Operating System" | cut -d ' ' -f5-)
+echo -e "  \e[$text Kernel               : $(uname -r)"
+echo -e "  \e[$text Total Amount Of Ram  : $tram MB"
+echo -e "  \e[$text Used RAM             : $uram MB"
+echo -e "  \e[$text Free RAM             : $fram MB"
+echo -e "  \e[$text System Uptime        : $uptime"
+echo -e "  \e[$text Ip Vps/Address       : $IPVPS"
+echo -e "  \e[$text Domain Name          : $domain\e[0m"
 echo -e ""
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;34m                       MENU                       \e[0m"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
+echo -e " \e[$line╒════════════════════════════════════════════════════════════╕\e[m"
+echo -e "  \e[$back_text                    \e[30m[\e[$box BANDWIDTH MONITORING\e[30m ]\e[1m                  \e[m"
+echo -e " \e[$line╘════════════════════════════════════════════════════════════╛\e[m"
+echo -e ""
+echo -e "   ${GB}Today ($DATE)     Monthly ($(date +%B/%Y))${NC}  "
+echo -e "     ${GB}↓↓ Down: $dtoday          ↓↓ Down: $dmon${NC}   "
+echo -e "     ${GB}↑↑ Up  : $utoday          ↑↑ Up  : $umon${NC}   "
+echo -e "     ${GB}≈ Total: $ttoday          ≈ Total: $tmon${NC}   "
+echo -e   ""
+echo -e " \e[$line╘════════════════════════════════════════════════════════════╛\e[m"
+echo -e " \e[$text Ssh/Ovpn   V2ray   Vless   Vlessxtls   Trojan-Ws   Trojan-Tls \e[0m "
+echo -e " \e[$below    $total_ssh         $vmess       $vless        $xtls           $trws           $trtls \e[0m "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "           ${WB}━━━━━ [ Total Config ] ━━━━━${NC}                "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e ""
+echo -e " \e[1;31m Vmess  : $vmess \e[0m "
+echo -e " \e[1;31m Vless  : $vless \e[0m "
+echo -e " \e[1;31m Trojan : $trws \e[0m "
+echo -e   ""
+echo -e " \e[$line╒════════════════════════════════════════════════════════════╕\e[m"
+echo -e "  \e[$back_text                        \e[30m[\e[$box VPS MENU\e[30m ]\e[1m                        \e[m"
+echo -e " \e[$line╘════════════════════════════════════════════════════════════╛\e[m"
 echo -e   ""
 echo -e "\e[1;36m 1 \e[0m: Menu SSH"
 echo -e "\e[1;36m 2 \e[0m: Menu Vmess"
@@ -108,12 +151,9 @@ echo -e "\e[1;36m 8 \e[0m: Clear RAM Cache"
 echo -e "\e[1;36m 9 \e[0m: Reboot VPS"
 echo -e "\e[1;36m x \e[0m: Exit Script"
 echo -e   ""
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;32m Client Name \e[0m: $Name"
-echo -e "\e[1;32m Expired     \e[0m: $Exp2"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e   ""
-echo -e "\e[1;36m --------------------TIM3VPN-------------------\e[0m"
+echo -e " \e[$line╒════════════════════════════════════════════════════════════╕\e[m"
+echo -e "  \e[$below Autoscript By        : TIM3VPN"
+echo -e " \e[$line╘════════════════════════════════════════════════════════════╛\e[m"
 echo -e   ""
 read -p " Select menu :  "  opt
 echo -e   ""
