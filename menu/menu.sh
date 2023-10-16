38ed5a7ca984deb1e8f3fe4c4610ebc34adb1f1a
RyukillerZ <42920480+RyukillerZ@users.noreply.github.com>
2023-10-16 10:40:50 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -24,6 +24,13 @@ NC='\e[0m'
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 LIGHT='\033[0;37m'
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+tyblue='\e[1;36m'
+purple='\e[0;35m'
+NC='\e[0m'
+
 # VPS Information
 #Domain
 domain=$(cat /etc/xray/domain)
@@ -38,10 +45,19 @@ tlsStatus=${remainingDays}
 if [[ ${remainingDays} -le 0 ]]; then
 	tlsStatus="expired"
 fi
+# TOTAL ACC CREATE VMESS WS
+vmess=$(grep -c -E "^### " "/etc/xray/config.json")
+# TOTAL ACC CREATE  VLESS WS
+vless=$(grep -c -E "^#& " "/etc/xray/config.json")
+# TOTAL ACC CREATE  VLESS TCP XTLS
+trws=$(grep -c -E "^#! " "/etc/xray/config.json")
+
 # OS Uptime
 uptime="$(uptime -p | cut -d " " -f 2-10)"
 # Download
 #Download/Upload today
+daily_usage=$(vnstat -d --oneline | awk -F\; '{print $6}' | sed 's/ //')
+monthly_usage=$(vnstat -m --oneline | awk -F\; '{print $11}' | sed 's/ //')
 dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
 utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
 ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
@@ -53,9 +69,14 @@ tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}
 dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')"
 umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
 tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
-# user
-Exp2=$"Lifetime"
-Name=$"givpn"
+# // xray status
+xray=$( systemctl status xray | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $xray == "running" ]]; then
+    status_xray="${GREEN}ON${NC}"
+else
+    status_xray="${RED}OFF${NC}"
+fi
+
 # Getting CPU Information
 cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
 cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
@@ -75,58 +96,54 @@ tram=$( free -m | awk 'NR==2 {print $2}' )
 uram=$( free -m | awk 'NR==2 {print $3}' )
 fram=$( free -m | awk 'NR==2 {print $4}' )
 clear 
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;34m                      VPS INFO                    \e[0m"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;32m OS            \e[0m: "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	
-echo -e "\e[1;32m Uptime        \e[0m: $uptime"
-echo -e "\e[1;32m Public IP     \e[0m: $IPVPS"
-echo -e "\e[1;32m Country       \e[0m: $LOC"
-#echo -e "\e[1;32m ASN           \e[0m: $ISP"
-#echo -e "\e[1;32m CITY          \e[0m: $CITY"
-echo -e "\e[1;32m DOMAIN        \e[0m: $domain"	
-echo -e "\e[1;32m DATE & TIME   \e[0m: $DATE2"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;34m                      RAM INFO                    \e[0m"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e ""
-echo -e "\e[1;32m RAM USED   \e[0m: $uram MB"	
-echo -e "\e[1;32m RAM TOTAL  \e[0m: $tram MB"
-echo -e ""
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;34m                       MENU                       \e[0m"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
+echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
+echo -e " \E[0;41;36m                 INFO SERVER                \E[0m"
+echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
+echo -e "  Cpu Model            :$cname"
+echo -e "  Cpu Frequency        :$freq MHz"
+echo -e "  Number Of Core       : $cores"
+echo -e "  CPU Usage            : $cpu_usage"
+echo -e "  Operating System     : "$(hostnamectl | grep "Operating System" | cut -d ' ' -f5-)
+echo -e "  Kernel               : $(uname -r)"
+echo -e "  Total Amount Of Ram  : $tram MB"
+echo -e "  Used RAM             : $uram MB"
+echo -e "  Free RAM             : $fram MB"
+echo -e "  System Uptime        : $uptime"
+echo -e "  Ip Vps/Address       : $IPVPS"
+echo -e "  Domain Name          : $domain\e[0m"
+echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
+echo -e "             [ XRAY-CORE${NC} : ${status_xray} ]         "
+echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
+echo -e "            \033[1;37mAutoScript BY TIM3VPN\033[0m"
+echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
+echo -e " \E[0;41;36m            BANDWIDTH MONITORING            \E[0m"
+echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
+echo -e " Daily Data Usage   :  ${yell}$daily_usage${N}"
+echo -e " Monthly Data Usage :  ${yell}$monthly_usage${N}"
 echo -e   ""
-echo -e "\e[1;36m 1 \e[0m: Menu SSH"
-echo -e "\e[1;36m 2 \e[0m: Menu Vmess"
-echo -e "\e[1;36m 3 \e[0m: Menu Vless"
-echo -e "\e[1;36m 4 \e[0m: Menu Trojan"
-echo -e "\e[1;36m 5 \e[0m: Menu Shadowsocks"
-echo -e "\e[1;36m 6 \e[0m: Menu Setting"
-echo -e "\e[1;36m 7 \e[0m: Status Service"
-echo -e "\e[1;36m 8 \e[0m: Clear RAM Cache"
-echo -e "\e[1;36m 9 \e[0m: Reboot VPS"
+echo -e "      \033[1;37m  Telegram Contact : @nemoecii\033[0m"
+echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
+echo -e " \E[0;41;36m                 MENU PANEL                 \E[0m"
+echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
+echo -e "\e[1;36m 1 \e[0m: Menu Vmess"
+echo -e "\e[1;36m 2 \e[0m: Menu Vless"
+echo -e "\e[1;36m 3 \e[0m: Menu Trojan"
+echo -e "\e[1;36m 4 \e[0m: Menu Setting"
+echo -e "\e[1;36m 5 \e[0m: Status Service"
+echo -e "\e[1;36m 6 \e[0m: Clear RAM Cache"
+echo -e "\e[1;36m 7 \e[0m: Reboot VPS"
 echo -e "\e[1;36m x \e[0m: Exit Script"
-echo -e   ""
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;32m Client Name \e[0m: $Name"
-echo -e "\e[1;32m Expired     \e[0m: $Exp2"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e   ""
-echo -e "\e[1;36m --------------------t.me/givpn-------------------\e[0m"
 echo -e   ""
 read -p " Select menu :  "  opt
 echo -e   ""
 case $opt in
-1) clear ; m-sshovpn ;;
-2) clear ; m-vmess ;;
-3) clear ; m-vless ;;
-4) clear ; m-trojan ;;
-5) clear ; m-ssws ;;
-6) clear ; m-system ;;
-7) clear ; running ;;
-8) clear ; clearcache ;;
-9) clear ; reboot ;;
+1) clear ; m-vmess ;;
+2) clear ; m-vless ;;
+3) clear ; m-trojan ;;
+4) clear ; m-system ;;
+5) clear ; running ;;
+6) clear ; clearcache ;;
+7) clear ; reboot ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; menu ;;
 esac
